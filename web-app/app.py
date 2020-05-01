e3f04992c737c7c0d2d985acdf88469aeb73b043
Horea Porutiu <horeaporutiu@gmail.com>
2020-05-01 15:25:50 -0700

    add working code

--- web-app/app.py
+++ web-app/app.py
@@ -13,34 +13,53 @@ from flask_wtf import FlaskForm
 app = Flask(__name__, instance_relative_config=False)
 app.secret_key = 'development key' #you will need a secret key
 
-#app.config.from_object('config.Config')
-
-
 if __name__ == "__main__":
   app.run()
 
 @app.route('/', methods=('GET', 'POST'))
+
+def startApp():
+    form = PredictForm()
+    return render_template('index.html', form=form)
+
+@app.route('/predict', methods=('GET', 'POST'))
 def predict():
-    print("I am here")
     form = PredictForm()
     if form.submit():
-        print("inside if statement")
+
         # NOTE: generate iam_token and retrieve ml_instance_id based on provided documentation
-        header = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + "PUT YOUR IAM Token here",
-                  'ML-Instance-ID': "PUT ML INSTANCE ID"}
-        python_object = [form.age.data, form.sex.data, str(form.bmi.data), form.children.data, form.smoker.data, form.region.data]
+        header = {'Content-Type': 'application/json', 'Authorization': 'Bearer '
+                 + " TODO: ADD YOUR IAM ACCESS TOKEN FROM IBM CLOUD HERE",
+                  'ML-Instance-ID': " TODO: ADD YOUR ML INSTANCE ID HERE "}
+
+        if(form.bmi.data == None): 
+          python_object = []
+        else:
+          python_object = [form.age.data, form.sex.data, float(form.bmi.data),
+            form.children.data, form.smoker.data, form.region.data]
         #Transform python objects to  Json
-        json_object = json.dumps(python_object)
+
+        userInput = []
+        userInput.append(python_object)
+
         # NOTE: manually define and pass the array(s) of values to be scored in the next line
-        payload_scoring = {"input_data": [{"fields": ["age", "sex", "bmi", "children", "smoker", "region"], "values": json_object }]}
+        payload_scoring = {"input_data": [{"fields": ["age", "sex", "bmi",
+          "children", "smoker", "region"], "values": userInput }]}
 
-        print("After payload statement")
-        response_scoring = requests.post("https://us-south.ml.cloud.ibm.com/v4/deployments/1ce09d1c-d84d-4b20-9069-f2a78f5a9b3a/predictions", json=payload_scoring, headers=header)
-        print("After response scoring  statement")
+        response_scoring = requests.post("https://us-south.ml.cloud.ibm.com/v4/deployments/ADD-DEPLOYMENT-ID-HERE/predictions", json=payload_scoring, headers=header)
 
-        print("Scoring response")
-        print(json.loads(response_scoring.text))
-        ab = json.loads(response_scoring.text)
-        form.abc= ab # this returns the response back to the front page
+        output = json.loads(response_scoring.text)
+        
+        for key in output:
+          ab = output[key]
+        
 
-    return render_template('index.html', form=form)
+        for key in ab[0]:
+          bc = ab[0][key]
+        
+        roundedCharge = round(bc[0][0],2)
+
+  
+        form.abc = roundedCharge # this returns the response back to the front page
+        return render_template('index.html', form=form)
+
